5563bc261e072bb073f01f3d8fc1dc545c91ccc7
Onelong <zzfeihua@163.com>
2016-12-15 00:27:26 +0800

    清理代码

--- HttpClient/src/main/java/com/ways2u/test/AppContext.java
+++
@@ -1,8 +0,0 @@
-package com.ways2u.test;
-
-/**
- * Created by huanglong on 2016/12/12.
- */
-public class AppContext {
-
-}
--- HttpClient/src/main/java/com/ways2u/test/Person.java
+++
@@ -1,9 +0,0 @@
-package com.ways2u.test;
-
-/**
- * Created by huanglong on 2016/12/13.
- */
-public class Person {
-    public String name = "name";
-    public int age = 30;
-}
